410c7179a11b718f3a09cf55037deac96fed6323
Leo Emeri <leo.emeri@gmail.com>
2023-12-19 17:50:36 +0100

    Testing auto deployment, changed header to WOPR by lem

--- src/app.svelte
+++ src/app.svelte
@@ -12,7 +12,7 @@
   <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container d-flex justify-content-between">
       <a href="/" class="navbar-brand d-flex align-items-center">
-        <strong>WOPR</strong>
+        <strong>WOPR by lem</strong>
       </a>
     </div>
   </div>
